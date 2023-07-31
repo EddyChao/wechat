b92af63c31fa4d115fa2af222a86d4d0e80c7472
EddyChao <74349287+EddyChao@users.noreply.github.com>
2023-07-31 13:42:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,20 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "http://t.weather.itboy.net/api/weather/city/" + city
   res = requests.get(url).json()
-  weather = res['data']['forecast'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  weatherHigh = res['data']['forecast'][0].high
+  weatherLow = res['data']['forecast'][0].low
+  weatherSunrise = res['data']['forecast'][0].sunrise
+  weatherSunset = res['data']['forecast'][0].sunset
+  weatherFx = res['data']['forecast'][0].fx
+  weatherFj = res['data']['forecast'][0].fj
+  weatherType = res['data']['forecast'][0].type
+  weatherNotice = res['data']['forecast'][0].notice
+  quality = res['data'].quality
+  shidu = res['data'].shidu
+  pm25 = res['data'].pm25
+  pm10 = res['data'].pm10
+  wendu = res['data'].wendu
+  return weatherHigh,weatherLow,weatherSunrise,weatherSunset,weatherFx,weatherFj,weatherType,weatherNotice,quality,shidu,pm25,pm10,wendu
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -47,7 +59,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+weatherHigh,weatherLow,weatherSunrise,weatherSunset,weatherFx,weatherFj,weatherType,weatherNotice,quality,shidu,pm25,pm10,wendu = get_weather()
+data = {"weatherHigh":{"value":weatherHigh,"color":get_random_color()},"weatherLow":{"value":weatherLow,"color":get_random_color()},"weatherSunrise":{"value":weatherSunrise,"color":get_random_color()},"weatherSunset":{"value":weatherSunset,"color":get_random_color()},"weatherFx":{"value":weatherFx,"color":get_random_color()},"weatherFj":{"value":weatherFj,"color":get_random_color()},"weatherType":{"value":weatherType,"color":get_random_color()},"weatherNotice":{"value":weatherNotice,"color":get_random_color()},"quality":{"value":quality,"color":get_random_color()},"shidu":{"value":shidu,"color":get_random_color()},"pm25":{"value":pm25,"color":get_random_color()},"pm10":{"value":pm10,"color":get_random_color()},"wendu":{"value":wendu,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
